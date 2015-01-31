708df2a32da75dc1d429125a1b09b338bfe44440
Ross Foley <ross3141@gmail.com>
2015-01-31 01:26:39 -0500

    Clean up unused files and folders

--- marioevolve.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#echo off
-export MYCLASSPATH=./properties
-for i in `ls ./lib/*.jar`
-do 
-	export MYCLASSPATH=${MYCLASSPATH}:${i}
-done
-echo ${MYCLASSPATH}
-nohup java -classpath ${MYCLASSPATH} mqp.anji.MarioEvolver mario.properties &
