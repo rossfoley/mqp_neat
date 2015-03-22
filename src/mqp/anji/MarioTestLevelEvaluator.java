b5fb8c31e1ee28fde40486da5a5cd3dae8d331d4
Ross Foley <ross3141@gmail.com>
2015-03-22 01:32:12 -0400

    Add TestLevelEvaluator

--- src/mqp/anji/MarioTestLevelEvaluator.java
+++ src/mqp/anji/MarioTestLevelEvaluator.java
@@ -8,7 +8,6 @@ import com.anji.util.DummyConfiguration;
 import com.anji.util.Properties;
 import mqp.mario.MQPMarioTask;
 import mqp.mario.NEATAgent;
-import mqp.mario.NEATCustomAgent;
 import org.jgap.Chromosome;
 import org.jgap.Configuration;
 
@@ -16,7 +15,7 @@ import org.jgap.Configuration;
  * Run a specific Mario AI agent through the MQPMarioTask
  * @author Ross Foley and Karl Kuhn
  */
-public class MarioActivator {
+public class MarioTestLevelEvaluator {
     public static void main(String[] args) throws Exception {
         // Load in the properties file
         Properties props = new Properties("mario.properties");
@@ -28,7 +27,7 @@ public class MarioActivator {
 
         // Set Mario options
         MarioAIOptions marioAIOptions = new MarioAIOptions();
-        marioAIOptions.setVisualization(true);
+        marioAIOptions.setVisualization(false);
         marioAIOptions.setFPS(fps);
         MQPMarioTask task = new MQPMarioTask(marioAIOptions);
 
@@ -38,8 +37,17 @@ public class MarioActivator {
         Activator a = activatorFactory.newActivator(c);
         NEATAgent marioAgent = new NEATAgent(a, radius);
 
-        // Evaluate the agent
-        task.evaluate(marioAgent);
+        // Set up difficulty and seed parameters
+        int difficulty = 1;
+        int startingSeed = 0;
+        int numLevels = 1000;
+
+        // Evaluate the agent on all the levels
+        System.out.println("Running champion with radius " + radius + " and difficulty " + difficulty);
+        for (int i = 0; i < numLevels; i++) {
+            float fitness = task.evaluateSingleLevel(marioAgent, difficulty, startingSeed + i);
+            System.out.println(fitness);
+        }
 
         // Exit
         System.exit(0);
